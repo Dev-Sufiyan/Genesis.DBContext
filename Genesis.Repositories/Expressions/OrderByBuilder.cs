b684a9d9e4e30d2fbb303e29747e13b54ac5c6f1
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-09-21 15:28:24 +0530

    ✨ Add Order By Expression Builder

---
+++ Genesis.Repositories/Expressions/OrderByBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Genesis.Models.DTO;
+using Genesis.Models.Enums;
+using System;
+using System.Linq.Expressions;
+
+namespace Genesis.Repositories.Expressions
+{
+    public class OrderByBuilder
+    {
+        public static Expression<Func<T, object>> BuildExpression<T>(string fieldName)
+        {
+            var parameter = Expression.Parameter(typeof(T), "x");
+            var property = typeof(T).GetProperty(fieldName);
+            if (property == null)
+                throw new InvalidDataException($"Invalid property name {fieldName} for class {typeof(T).Name}");
+
+            var propertyExp = Expression.MakeMemberAccess(parameter, property);
+            var conversion = Expression.Convert(propertyExp, typeof(object)); // casting to object for compatibility
+
+            return Expression.Lambda<Func<T, object>>(conversion, parameter);
+        }
+    }
+}
