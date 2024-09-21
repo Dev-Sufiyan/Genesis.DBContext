97acd72b2ab364e7ff8468da0e71ff8fb3de333d
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-09-21 15:30:09 +0530

    ✨ Add Query Builder

---
+++ Genesis.Repositories/Extensions/QueryBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Genesis.Models.DTO;
+using Genesis.Repositories.Expressions;
+using System.Linq.Expressions;
+
+namespace Genesis.Repositories.Extensions
+{
+    public static class QueryBuilder
+    {
+        public static IQueryable<T> AddSearchParams<T>(this IQueryable<T> query, SearchParams searchParams)
+        {
+            Expression<Func<T, bool>> filterExpression = FilterBuilder.BuildExpression<T>(searchParams.Filters);
+
+            query = query.Where(filterExpression);
+
+            if (!string.IsNullOrEmpty(searchParams.OrderBy))
+            {
+                Expression<Func<T, object>> orderByExpression = OrderByBuilder.BuildExpression<T>(searchParams.OrderBy);
+                query = searchParams.IsDescending ? query.OrderByDescending(orderByExpression) : query.OrderBy(orderByExpression);
+            }
+
+            if (searchParams.Pagination != null)
+            {
+                var pagination = searchParams.Pagination;
+                query = query.Skip((pagination.CurrentPage ?? 1 - 1) * pagination.RecordsPerPage)
+                             .Take(pagination.RecordsPerPage);
+            }
+
+            return query;
+        }
+    }
+}
