b3ecf6b4583b845f7f54a57f6bb433b097fabcaf
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-09-21 15:29:27 +0530

    ✨ Add condition for or & Add string methods

--- Genesis.Repositories/Expressions/FilterBuilder.cs
+++ Genesis.Repositories/Expressions/FilterBuilder.cs
@@ -5,22 +5,23 @@ using System.Reflection;
 
 namespace Genesis.Repositories.Expressions
 {
-    public static class ComparisonBuilder
+    public static class FilterBuilder
     {
-        public static Expression<Func<T, bool>> BuildExpression<T>(List<SearchParam> searchParam)
+        public static Expression<Func<T, bool>> BuildExpression<T>(List<SearchFilters>? SearchFilters, JoinOperator joinOperator = JoinOperator.JointAnd)
         {
-            if (searchParam == null || searchParam.Count == 0)
+            if (SearchFilters == null || SearchFilters.Count == 0)
             {
                 return x => true;
             }
 
-            var initialExpression = BuildExpression<T>(searchParam[0]);
+            var initialExpression = GetExpression<T>(SearchFilters[0]);
             var parameter = initialExpression.Parameters[0];
 
-            foreach (var searchObj in searchParam.Skip(1))
+            foreach (var searchObj in SearchFilters.Skip(1))
             {
-                var newExpression = BuildExpression<T>(searchObj);
-                initialExpression = CombineExpressions(initialExpression, newExpression, Expression.AndAlso);
+                var newExpression = GetExpression<T>(searchObj);
+                initialExpression = CombineExpressions(initialExpression, newExpression,
+                                                        joinOperator == JoinOperator.JointAnd ? Expression.AndAlso : Expression.OrElse);
             }
 
             return initialExpression;
@@ -39,21 +40,21 @@ namespace Genesis.Repositories.Expressions
             return Expression.Lambda<Func<T, bool>>(combinedBody, parameter);
         }
 
-        public static Expression<Func<T, bool>> BuildExpression<T>(SearchParam searchParam)
+        public static Expression<Func<T, bool>> GetExpression<T>(SearchFilters SearchFilters)
         {
             var parameter = Expression.Parameter(typeof(T), "x");
 
-            var property = typeof(T).GetProperty(searchParam.Field);
+            var property = typeof(T).GetProperty(SearchFilters.Field);
             if (property == null)
-                throw new InvalidDataException($"Invalid property name {searchParam.Field} for class {typeof(T).Name}");
+                throw new InvalidDataException($"Invalid property name {SearchFilters.Field} for class {typeof(T).Name}");
 
             var propertyType = property.PropertyType;
 
             var expLeft = Expression.MakeMemberAccess(parameter, property);
-            var propertyValue = ConvertToType(propertyType, searchParam.Value);
+            var propertyValue = ConvertToType(propertyType, SearchFilters.Value);
             var expRight = Expression.Constant(propertyValue, propertyType);
 
-            Expression comparison = searchParam.Comparator switch
+            Expression comparison = SearchFilters.Comparator switch
             {
                 ComparisonOperator.Equal => Expression.Equal(expLeft, expRight),
                 ComparisonOperator.NotEqual => Expression.NotEqual(expLeft, expRight),
@@ -61,13 +62,24 @@ namespace Genesis.Repositories.Expressions
                 ComparisonOperator.GreaterThanOrEqual => Expression.GreaterThanOrEqual(expLeft, expRight),
                 ComparisonOperator.LessThan => Expression.LessThan(expLeft, expRight),
                 ComparisonOperator.LessThanOrEqual => Expression.LessThanOrEqual(expLeft, expRight),
-                ComparisonOperator.StartsWith => propertyType == typeof(string)
-                               ? Expression.Call(expLeft, typeof(string).GetMethod("StartsWith", new[] { typeof(string) }) ?? throw new InvalidOperationException("StartsWith method not found"), expRight)
-                               : throw new InvalidOperationException("StartsWith operator is only valid for string properties."),
-                _ => throw new NotImplementedException($"Comparison operator '{searchParam.Comparator}' is not implemented.")
+                ComparisonOperator.StartsWith => CreateStringMethodCall("StartsWith", expLeft, expRight, propertyType),
+                ComparisonOperator.EndsWith => CreateStringMethodCall("EndsWith", expLeft, expRight, propertyType),
+                ComparisonOperator.Contains => CreateStringMethodCall("Contains", expLeft, expRight, propertyType),
+                _ => throw new NotImplementedException($"Comparison operator '{SearchFilters.Comparator}' is not implemented.")
             };
 
             return Expression.Lambda<Func<T, bool>>(comparison, parameter);
+        }
+
+        private static Expression CreateStringMethodCall(string methodName, Expression expLeft, Expression expRight, Type propertyType)
+        {
+            if (propertyType != typeof(string))
+            {
+                throw new InvalidOperationException($"{nameof(SearchFilters.Comparator)} operator is only valid for string properties.");
+            }
+
+            return Expression.Call(expLeft, typeof(string).GetMethod(methodName, new[] { typeof(string) })
+                ?? throw new InvalidOperationException($"{methodName} method not found"), expRight);
         }
 
         public static object ConvertToType(Type targetType, string value)
